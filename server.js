a34b58d6ab2521a31729f236b72d5570d06a848b
Moeinvir <moin.kb1388@gmail.com>
2025-07-18 17:32:24 +0330

    Vagea ridam to in zendegi

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 const app = express(); 
 
+const DATA_FILE = path.join(__dirname, 'users.json');
+
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
@@ -16,9 +18,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-const DATA_FILE = path.join(__dirname, 'users.json');
 
 function readUsers() {
   if (!fs.existsSync(DATA_FILE)) {
